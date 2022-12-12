6ed57774854715c9fe90d44f5b49161051217537
Ivan Cejic <ivancejic5@gmail.com>
2022-12-12 16:32:08 +0100

    Dodat je jos jedan red za cekanje 'deleteQ'. Moze daa se desi da se pomocu funkcije 'truncat' brise vise karaktera nego sto je trenutno u baferu(memoriji). Mora da ceka sve dok se memorija ne napune do granice koja je odredjena od strane truncat funkcije.

--- domaci2/Stred.c
+++ domaci2/Stred.c
@@ -10,6 +10,7 @@
 #include <linux/uaccess.h>
 #include <linux/errno.h>
 #include <linux/device.h>
+#include <linux/semaphore.h>
 #define BUFF_SIZE 100
 MODULE_LICENSE("Dual BSD/GPL");
 
@@ -18,12 +19,15 @@ static struct class *my_class;
 static struct device *my_device;
 static struct cdev *my_cdev;
 DECLARE_WAIT_QUEUE_HEAD(writeQ);
-
+DECLARE_WAIT_QUEUE_HEAD(deleteQ);
 
 
 char memory[BUFF_SIZE];
 int pos = 0;
 int endRead = 0;
+struct semaphore sem;
+
+
 
 int stred_open(struct inode *pinode, struct file *pfile);
 int stred_close(struct inode *pinode, struct file *pfile);
@@ -61,7 +65,7 @@ ssize_t stred_read(struct file *pfile, char __user *buffer, size_t length, loff_
 		endRead = 0;
 		return 0;
 	}
-	len = scnprintf(buff, BUFF_SIZE, "%s", memory);
+	len = scnprintf(buff, BUFF_SIZE, "%s\n", memory);
 	ret = copy_to_user(buffer, buff, len);
 	endRead=1;
 	return len;
@@ -84,24 +88,30 @@ ssize_t stred_write(struct file *pfile, const char __user *buffer, size_t length
 
 	ret = sscanf(buff,"%10[^= ]=%99[^\t\n=]", func, input_string);
 
+
+
+//kriticna sekcija je bilo koja od funkcija ispod... 
+
+	
+
 	if(!strcmp(func,"string")){
 		pos=0;
 		len = strlen(input_string);
 		strncpy(memory,input_string,len);
 		pos = len;	
 		memory[pos]='\0';
-		printk(KERN_INFO "Vrsi se funkcija string\n");
+		printk(KERN_INFO "String function completed!\n");
 	}
 
 	if(!strcmp(func,"clear")){
+
 		memory[0] = '\0';
-		printk(KERN_INFO "Vrsi se funkcija clear\n");
+		printk(KERN_INFO "Clear function completed!\n");
 		wake_up_interruptible(&writeQ);
 	}
 
 	if(!strcmp(func,"shrink")){
 		printk(KERN_INFO "Vrsi se funkcija shrink \n ");
-		printk(KERN_INFO "Trenutna vrednost memorije:pocetak%skraj", memory);
 		if(memory[0] == ' '){
 			printk(KERN_INFO "Postoji space na pocetku \n");
 			skip_spaces(memory);
@@ -111,7 +121,7 @@ ssize_t stred_write(struct file *pfile, const char __user *buffer, size_t length
 			strim(memory);
 		}
 		if(memory[0] == ' '){
-			printk(KERN_INFO "ipak postoji razmak na pocetku \n");
+			printk(KERN_INFO "idalje postoji razmak na pocetku \n");
 		}
 		if(memory[strlen(memory)-1]  == ' '){
 			printk(KERN_INFO "ipak postoji razmak na kraju \n");
@@ -119,7 +129,6 @@ ssize_t stred_write(struct file *pfile, const char __user *buffer, size_t length
 		wake_up_interruptible(&writeQ);
 	}
 	if(!strcmp(func,"append")){
-		printk(KERN_INFO "Vrsi se funkcija append");		
 		len = strlen(input_string);
 		printk(KERN_INFO "Ako bi se upisao string, duzina bi bila: %d", len+strlen(memory));		
 		if(wait_event_interruptible(writeQ, strlen(memory)+len < 100)) {
@@ -127,17 +136,20 @@ ssize_t stred_write(struct file *pfile, const char __user *buffer, size_t length
 		}
 		if(strlen(memory)+len<100){		
 			strncat(memory, input_string,len);
+			printk(KERN_INFO "Append function completed!\n");		
 		}
+		wake_up_interruptible(&deleteQ);
 
 	}
 	if(!strcmp(func,"truncat")){ 
 		int l = (int) simple_strtoul(input_string,NULL,10);
+		if(wait_event_interruptible(deleteQ,strlen(memory)-l>0))
+			return -ERESTARTSYS;
 		memmove(memory+strlen(memory)-l,"\0",1);
-		printk(KERN_INFO "Vrsi se funkcija truncate\n");
+		printk(KERN_INFO "Truncat function completed!\n");
 		wake_up_interruptible(&writeQ);
 	}
 	if(!strcmp(func,"remove")){
-		printk(KERN_INFO "Vrsi se funkcija remove");
 		char *substring_ptr = strstr(memory,input_string);
 			
 		if(substring_ptr){
@@ -145,6 +157,7 @@ ssize_t stred_write(struct file *pfile, const char __user *buffer, size_t length
 				memmove(substring_ptr, substring_ptr + strlen(input_string),strlen(substring_ptr+strlen(input_string))+1);
 				substring_ptr = strstr(memory,input_string);
 			}
+			printk(KERN_INFO "Remove function completed!\n");
 			wake_up_interruptible(&writeQ);
 			
 		}
@@ -190,7 +203,7 @@ static int __init stred_init(void)
 	}
    printk(KERN_INFO "cdev added\n");
    printk(KERN_INFO "Hello world\n");
-
+   sema_init(&sem,1);
    return 0;
 
    fail_2:
